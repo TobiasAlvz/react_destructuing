8e1eb57187606ad89253434d52ecb146f9afab1d
Tobias Alves <tobiasjose12@gmail.com>
2024-05-02 09:46:31 -0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const animals = [{name: 'cat', sound: 'meow'}, {name: 'dog', sound: 'woof'}];
+
+const [cat, dog] = animals;
+
+console.log (cat);
+
+const {name: a, sound} = cat;
+
+console.log (a);
+ 
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import cars from './cars';
+const [honda, tesla] = cars; 
+
+ReactDOM.render (
+  <table>
+    <thead>
+      <tr>
+        <th>Modelo</th>
+        <th>Cor</th>
+        <th>Velocidade</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{honda.color}</td>
+        <td>{honda.speed.max} km/h</td>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{tesla.color}</td>
+        <td>{tesla.speed.max} km/h</td>
+      </tr>
+    </tbody>
+  </table>,
+  document.getElementById ('root')
+);
